f8c2d576e43f31049bea737b5043086f3ae94120
mar4enkom <mar44enkom@gmail.com>
2026-01-30 10:42:34 +0100

    solve problem_mm

---
+++ 7-linear-search-and-simple-sorting/problem_mm.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main() {
+    unsigned n; 
+    scanf("%u", &n);
+    int arr[n];
+
+    for (int i=0; i<n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min = INT_MAX;
+    int max = INT_MIN;    
+    for (int i=0; i<n; ++i) {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+    }
+
+    printf("%d %d", min, max);
+}
